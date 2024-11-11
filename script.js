31eb9a0ed733900db80ccfd8c2f5401148eebc5f
ermmme <185312182+ermmme@users.noreply.github.com>
2024-11-10 22:17:04 -0800

    FInished tasks 2 and 3.

--- script.js
+++ script.js
@@ -23,6 +23,20 @@ console.log(customRange(start, end));
 // Example output:
 // console.log(sum(range(1, 10))); // returns 55
 
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function calculateSum(arr) {
+  let sumTotal = 0;
+
+  for (let index = 0; index < arr.length; index++) {
+    sumTotal += arr[index];
+  }
+
+  return sumTotal;
+}
+
+console.log(calculateSum(numbersArray));
+
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
 // its elements appear. For this exercise, write two functions, reverseArray and
 // reverseArrayInPlace. The first, reverseArray, takes an array as an argument and
@@ -36,3 +50,31 @@ console.log(customRange(start, end));
 // let arrayValue = [1, 2, 3, 4, 5];
 // reverseArrayInPlace(arrayValue);
 // console.log(arrayValue); // outputs [5, 4, 3, 2, 1]
+
+function invertArray(originalArray) {
+  let flippedArray = [];
+  for (let index = originalArray.length - 1; index >= 0; index--) {
+    flippedArray.push(originalArray[index]);
+  }
+  return flippedArray;
+}
+
+function reverseArrayDirectly(modifiableArray) {
+  let leftIndex = 0;
+  let rightIndex = modifiableArray.length - 1;
+
+  while (leftIndex < rightIndex) {
+    let temp = modifiableArray[leftIndex];
+    modifiableArray[leftIndex] = modifiableArray[rightIndex];
+    modifiableArray[rightIndex] = temp;
+
+    leftIndex++;
+    rightIndex--;
+  }
+}
+
+console.log(invertArray(["A", "B", "C"]));
+
+let arrayExample = [1, 2, 3, 4, 5];
+reverseArrayDirectly(arrayExample);
+console.log(arrayExample);
